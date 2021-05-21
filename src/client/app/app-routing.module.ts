50afccf4dc41e13a89faae8b444321592fdd3619
NGK <59110984+nandu-g-krishnan@users.noreply.github.com>
2021-05-22 00:47:22 +0530

    complete project

---
+++ src/client/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { AuthGuard } from './auth.guard';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'contacts',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'contacts',
+    component: ContactListComponent,
+    canActivate:[AuthGuard]
+  },
+  {
+    path: 'new',
+    component: AddContactComponent,
+    canActivate:[AuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'contacts'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
